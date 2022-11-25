809313d6a4b9d585a17fe2755e7e4fc81e591983
Alexander Kastler <github@xaka.eu>
2022-11-25 20:35:59 +0100

    Introduce ToPrettyIBAN

--- src/Xan.Extensions/StringExtensions.cs
+++ src/Xan.Extensions/StringExtensions.cs
@@ -1,7 +1,27 @@
-﻿namespace Xan.Extensions;
+﻿using System.Text;
+
+namespace Xan.Extensions;
 
 public static class StringExtensions
 {
+    public static string ToPrettyIBAN(this string? iban)
+    {
+        if (string.IsNullOrEmpty(iban))
+        {
+            return string.Empty;
+        }
+        StringBuilder sb = new();
+        for (int index = 0; index < iban.Length; index++)
+        {
+            if (index > 0 && index % 4 == 0)
+            {
+                sb.Append(' ');
+            }
+            sb.Append(iban[index]);
+        }
+        return sb.ToString();
+    }
+
     public static string Truncate(this string input, int maxLength)
     {
         if (string.IsNullOrEmpty(input))
---
+++ test/Xan.Extensions.Tests/StringExtensionsTests/ToPrettyIBAN.cs
@@ -0,0 +1,14 @@
+﻿namespace Xan.Extensions.Tests.StringExtensionsTests;
+
+public class ToPrettyIBAN
+{
+    [Theory]
+    [InlineData("", null)]
+    [InlineData("", "")]
+    [InlineData("ATpp bbbb bkkk kkkk kkkk", "ATppbbbbbkkkkkkkkkkk")]
+    [InlineData("EGpp bbbb ssss kkkk kkkk kkkk kkkk k", "EGppbbbbsssskkkkkkkkkkkkkkkkk")]
+    public void Input_ReturnsExpected(string expected, string input)
+    {
+        Assert.Equal(expected, input.ToPrettyIBAN());
+    }
+}
