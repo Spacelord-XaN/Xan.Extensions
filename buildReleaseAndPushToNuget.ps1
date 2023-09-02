d0f44a5082d889917c67614a676ab69c81911407
Alexander Kastler <github@xaka.eu>
2023-09-02 13:02:47 +0200

    Add PowerShell script to publish nuget package

---
+++ buildReleaseAndPushToNuget.ps1
@@ -0,0 +1,10 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$version,
+
+    [Parameter(Mandatory=$true)]
+    [string]$apiKey
+)
+dotnet build .\Xan.Extensions.sln --configuration Release
+dotnet pack .\Xan.Extensions.sln --configuration Release
+dotnet nuget push .\src\Xan.Extensions\bin\Release\Xan.Extensions.$version.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
