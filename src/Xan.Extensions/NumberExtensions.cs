6c8511753be0986627ace134e9e9bbfe5c73be2d
Alexander Kastler <github@xaka.eu>
2023-10-28 07:26:17 +0200

    Add NumberExtensions IsInRange and IsBetween

---
+++ src/Xan.Extensions/NumberExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Xan.Extensions;
+
+public static class NumberExtensions
+{
+    public static bool IsBetween<T>(this T number, T minExclusive, T maxExclusive)
+        where T : INumber<T>
+    {
+        return number > minExclusive && number < maxExclusive;
+    }
+
+    public static bool IsInRange<T>(this T number, T minInclusive, T maxInclusive)
+        where T : INumber<T>
+    {
+        return number >= minInclusive && number <= maxInclusive;
+    }
+}
---
+++ test/Xan.Extensions.Tests/NumberExtensionTests/IsBetween.cs
@@ -0,0 +1,32 @@
+﻿namespace Xan.Extensions.Tests.NumberExtensionTests;
+
+public class IsBetween
+{
+    [Theory]
+    [InlineData(44, 23, 66, true)]
+    [InlineData(4, 4, 6, false)]
+    public void Int(int value, int min, int max, bool expectedResult)
+    {
+        //  Arrange
+
+        //  Act
+        bool result = value.IsBetween(min, max);
+
+        //  Assert
+        result.Should().Be(expectedResult);
+    }
+
+    [Theory]
+    [InlineData(4.4, 2.3, 6.6, true)]
+    [InlineData(4.4, 4.5, 6.6, false)]
+    public void Double(double value, double min, double max, bool expectedResult)
+    {
+        //  Arrange
+
+        //  Act
+        bool result = value.IsBetween(min, max);
+
+        //  Assert
+        result.Should().Be(expectedResult);
+    }
+}
