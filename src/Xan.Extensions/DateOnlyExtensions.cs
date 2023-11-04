907eb26c1722f2be106a204de8390364d731e226
Alexander Kastler <github@xaka.eu>
2023-11-04 18:32:35 +0100

    StartOfWeek now returns correct values for all possible first day of weeks

--- src/Xan.Extensions/DateOnlyExtensions.cs
+++ src/Xan.Extensions/DateOnlyExtensions.cs
@@ -14,14 +14,94 @@ public static class DateOnlyExtensions
         return date.StartOfDay().AddDays(1).AddMilliseconds(-1);
     }
 
+    private static readonly Dictionary<DayOfWeek, Dictionary<DayOfWeek, int>> _startOfWeekOffsets = new()
+    {
+        { DayOfWeek.Monday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, 0 },
+                { DayOfWeek.Tuesday, -1 },
+                { DayOfWeek.Wednesday, -2 },
+                { DayOfWeek.Thursday, -3 },
+                { DayOfWeek.Friday, -4 },
+                { DayOfWeek.Saturday, -5 },
+                { DayOfWeek.Sunday, -6 },
+            }
+        },
+        { DayOfWeek.Tuesday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -6 },
+                { DayOfWeek.Tuesday, 0 },
+                { DayOfWeek.Wednesday, -1 },
+                { DayOfWeek.Thursday, -2 },
+                { DayOfWeek.Friday, -3 },
+                { DayOfWeek.Saturday, -4 },
+                { DayOfWeek.Sunday, -5 },
+            }
+        },
+        { DayOfWeek.Wednesday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -5 },
+                { DayOfWeek.Tuesday, -6 },
+                { DayOfWeek.Wednesday, 0 },
+                { DayOfWeek.Thursday, -1 },
+                { DayOfWeek.Friday, -2 },
+                { DayOfWeek.Saturday, -3 },
+                { DayOfWeek.Sunday, -4 },
+            }
+        },
+        { DayOfWeek.Thursday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -4 },
+                { DayOfWeek.Tuesday, -5 },
+                { DayOfWeek.Wednesday, -6 },
+                { DayOfWeek.Thursday, 0 },
+                { DayOfWeek.Friday, -1 },
+                { DayOfWeek.Saturday, -2 },
+                { DayOfWeek.Sunday, -3 },
+            }
+        },
+        { DayOfWeek.Friday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -3 },
+                { DayOfWeek.Tuesday, -4 },
+                { DayOfWeek.Wednesday, -5 },
+                { DayOfWeek.Thursday, -6 },
+                { DayOfWeek.Friday, 0 },
+                { DayOfWeek.Saturday, -1 },
+                { DayOfWeek.Sunday, -2 },
+            }
+        },
+        { DayOfWeek.Saturday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -2 },
+                { DayOfWeek.Tuesday, -3 },
+                { DayOfWeek.Wednesday, -4 },
+                { DayOfWeek.Thursday, -5 },
+                { DayOfWeek.Friday, -6 },
+                { DayOfWeek.Saturday, 0 },
+                { DayOfWeek.Sunday, -1 },
+            }
+        },
+        { DayOfWeek.Sunday, new Dictionary<DayOfWeek, int>
+            {
+                { DayOfWeek.Monday, -1 },
+                { DayOfWeek.Tuesday, -2 },
+                { DayOfWeek.Wednesday, -3 },
+                { DayOfWeek.Thursday, -4 },
+                { DayOfWeek.Friday, -5 },
+                { DayOfWeek.Saturday, -6 },
+                { DayOfWeek.Sunday, 0 },
+            }
+        }
+    };
+
     public static DateTime StartOfWeek(this DateOnly date)
     {
         CultureInfo cultureInfo = CultureInfo.CurrentCulture;
         DayOfWeek firstDayOfWeek = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+        int offsetDays = _startOfWeekOffsets[firstDayOfWeek][date.DayOfWeek];
 
-        int offsetDays = (int)date.DayOfWeek - (int)firstDayOfWeek;
-
-        return date.StartOfDay().AddDays(-offsetDays);
+        return date.StartOfDay().AddDays(offsetDays);
     }
 
     public static DateTime EndOfWeek(this DateOnly date)
--- test/Xan.Extensions.Tests/DateOnlyExtensionsTests/StartOfWeek.cs
+++ test/Xan.Extensions.Tests/DateOnlyExtensionsTests/StartOfWeek.cs
@@ -10,22 +10,25 @@ public sealed class StartOfWeek
         _austrianCulture.Dispose();
     }
 
-    [Fact]
-    public void ReturnsDateTimeWithCorrectTime()
+    [Theory, ClassData(typeof(ReturnsDateTimeWithCorrectTimeTestCases))]
+    public void ReturnsDateTimeWithCorrectTime(DateOnly date, DateTime expectedResult)
     {
         //  Arrange
-        DateOnly date = new(2023, 04, 05);
 
         //  Act
         DateTime result = date.StartOfWeek();
 
-        //  Assert
-        result.Year.Should().Be(date.Year);
-        result.Month.Should().Be(date.Month);
-        result.Day.Should().Be(3);
-        result.Hour.Should().Be(0);
-        result.Minute.Should().Be(0);
-        result.Second.Should().Be(0);
-        result.Millisecond.Should().Be(0);
+        //  Assert\
+        result.Should().Be(expectedResult);
+    }
+
+    private class ReturnsDateTimeWithCorrectTimeTestCases :
+        TheoryData<DateOnly, DateTime>
+    {
+        public ReturnsDateTimeWithCorrectTimeTestCases()
+        {
+            Add(new DateOnly(2023, 04, 05), new DateTime(2023, 04, 03));
+            Add(new DateOnly(2023, 10, 15), new DateTime(2023, 10, 09));
+        }
     }
 }
