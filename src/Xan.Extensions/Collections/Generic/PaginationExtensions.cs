0139aa6ac00b8108bac3a9c5593b018a19e60acb
Alexander Kastler <github@xaka.eu>
2024-03-31 10:54:27 +0200

    Add AsPaginatedAsync for IEnumerable<T> extension methods

---
+++ src/Xan.Extensions/Collections/Generic/PaginationExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace Xan.Extensions.Collections.Generic;
+
+public static class PaginationExtensions
+{
+    public static async Task<IPaginatedList<T>> AsPaginatedAsync<T>(this IEnumerable<T> enumerable, int pageSize, int pageIndex)
+    {
+        ArgumentNullException.ThrowIfNull(enumerable);
+
+        return await enumerable.AsPaginatedAsync(pageSize, pageIndex, x => x);
+    }
+
+    public static async Task<IPaginatedList<TResult>> AsPaginatedAsync<TSource, TResult>(this IEnumerable<TSource> enumerable, int pageSize, int pageIndex, Func<TSource, TResult> selector)
+    {
+        ArgumentNullException.ThrowIfNull(enumerable);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        return await enumerable.AsPaginatedAsync(pageSize, pageIndex, async source => await Task.FromResult(selector(source)));
+    }
+
+    public static async Task<IPaginatedList<TResult>> AsPaginatedAsync<TSource, TResult>(this IEnumerable<TSource> enumerable, int pageSize, int pageIndex, Func<TSource, Task<TResult>> selectorAsync)
+    {
+        ArgumentNullException.ThrowIfNull(enumerable);
+        ArgumentNullException.ThrowIfNull(selectorAsync);
+
+        if (pageIndex == 0)
+        {
+            pageIndex = 1;
+        }
+
+        int totalItemCount = enumerable.Count();
+        int totalPageCount = IPaginated.CalcTotalPages(totalItemCount, pageSize);
+        pageIndex = Math.Min(pageIndex, Math.Max(1, totalPageCount));
+
+        IEnumerable<TSource> paginatedEnumerable = enumerable;
+        if (pageSize != IPaginated.AllPageSize)
+        {
+            int skipCount = Math.Max(pageIndex - 1, 0) * pageSize;
+            paginatedEnumerable = enumerable.Skip(skipCount).Take(pageSize);
+        }
+        IReadOnlyList<TSource> sourceItems = paginatedEnumerable.ToArray();
+        List<TResult> resultItems = [];
+        foreach (TSource sourceItem in sourceItems)
+        {
+            resultItems.Add(await selectorAsync(sourceItem));
+        }
+        return new PaginatedList<TResult>(resultItems, pageIndex, pageSize, totalPageCount, totalItemCount);
+    }
+}
---
+++ test/Xan.Extensions.Tests/Collections/Generic/PaginationExtensionsTests/AsPaginatedAsync.cs
@@ -0,0 +1,81 @@
+﻿using Xan.Extensions.Collections.Generic;
+
+namespace Xan.Extensions.Tests.Collections.Generic.PaginationExtensionsTests;
+
+public class AsPaginatedAsync
+{
+    private class TestEntity
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+    }
+
+    private readonly Fixture _fixture = new();
+
+    [Fact]
+    public async Task ShouldReturnCorrectPageSize()
+    {
+        //  Arrange
+        IEnumerable<TestEntity> testEntities = _fixture.Build<TestEntity>().CreateMany(99);
+
+        //  Act
+        IPaginatedList<TestEntity> result = await testEntities.AsPaginatedAsync(10, 2);
+
+        //  Assert
+        using (new AssertionScope())
+        {
+            result.HasNextPage.Should().BeTrue();
+            result.HasPreviousPage.Should().BeTrue();
+            result.PageIndex.Should().Be(2);
+            result.PageSize.Should().Be(10);
+            result.TotalItemCount.Should().Be(99);
+            result.TotalPageCount.Should().Be(10);
+            result.Should().BeEquivalentTo(testEntities.Skip(10).Take(10));
+        }
+    }
+
+    [Fact]
+    public async Task WithSelector_ShouldReturnCorrectPageSize()
+    {
+        //  Arrange
+        IEnumerable<TestEntity> testEntities = _fixture.Build<TestEntity>().CreateMany(99);
+
+        //  Act
+        IPaginatedList<string?> result = await testEntities.AsPaginatedAsync(10, 2, entity => entity.Name);
+
+        //  Assert
+        using (new AssertionScope())
+        {
+            result.HasNextPage.Should().BeTrue();
+            result.HasPreviousPage.Should().BeTrue();
+            result.PageIndex.Should().Be(2);
+            result.PageSize.Should().Be(10);
+            result.TotalItemCount.Should().Be(99);
+            result.TotalPageCount.Should().Be(10);
+            result.Should().BeEquivalentTo(testEntities.Skip(10).Take(10).Select(x => x.Name));
+        }
+    }
+
+    [Fact]
+    public async Task WithAsyncSelector_ShouldReturnCorrectPageSize()
+    {
+        //  Arrange
+        IEnumerable<TestEntity> testEntities = _fixture.Build<TestEntity>().CreateMany(99);
+
+        //  Act
+        IPaginatedList<string?> result = await testEntities.AsPaginatedAsync(10, 2, async entity => await Task.FromResult(entity.Name));
+
+        //  Assert
+        using (new AssertionScope())
+        {
+            result.HasNextPage.Should().BeTrue();
+            result.HasPreviousPage.Should().BeTrue();
+            result.PageIndex.Should().Be(2);
+            result.PageSize.Should().Be(10);
+            result.TotalItemCount.Should().Be(99);
+            result.TotalPageCount.Should().Be(10);
+            result.Should().BeEquivalentTo(testEntities.Skip(10).Take(10).Select(x => x.Name));
+        }
+    }
+}
